45b927d47fc54a289d513d6c5022ff210d3fd425
kimbi91 <subscribefan@gmail.com>
2022-07-09 12:08:40 +0200

    DB creation with bonus

--- tasks/db_creation.sql
+++ tasks/db_creation.sql
@@ -44,6 +44,43 @@ Kritériumok az adatbázissal kapcsolatban:
     - az adatbázis, a táblák és a mezők elnevezése rajtad áll, azonban használj angol megnevezéseket, méghozzá következetesen
       (ha az egyik táblában camel-case szerinti mező-neveket adtál, akkor a másik táblában is tégy úgy)
 
+
+    CREATE TABLE `messenger_app`.`user_info`
+    ( `id` INT NOT NULL AUTO_INCREMENT ,
+        `name` VARCHAR(100) NOT NULL ,
+        `email` VARCHAR(100) NULL DEFAULT NULL ,
+        `password` VARCHAR(16) NOT NULL ,
+        `isActive` BOOLEAN NOT NULL ,
+        `dateOfRegistration` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+        PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+    CREATE TABLE `messenger_app`.`message_info`
+    ( `id` INT NOT NULL AUTO_INCREMENT ,
+        `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+        `message` TEXT NOT NULL ,
+        PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+    CREATE TABLE `messenger_app`.`message_user`
+    ( `fromID` INT NOT NULL ,
+        `toID` INT NOT NULL ,
+        `messageID` INT NOT NULL ,
+        `isReply` BOOLEAN NOT NULL DEFAULT FALSE ,
+        PRIMARY KEY (`fromID`, `toID`, `messageID`)) ENGINE = InnoDB;
+
+    ALTER TABLE `messenger_app`.`message_user`
+    ADD CONSTRAINT fk_fromID
+    FOREIGN KEY (fromID)
+    REFERENCES `user_info`(`id`);
+
+    ALTER TABLE `messenger_app`.`message_user`
+    ADD CONSTRAINT fk_toID
+    FOREIGN KEY (toID)
+    REFERENCES `user_info`(`id`);
+
+    ALTER TABLE `messenger_app`.`message_user`
+    ADD CONSTRAINT fk_messageID
+    FOREIGN KEY (messageID)
+    REFERENCES `message_info`(`id`);
 */
 
 
@@ -54,5 +91,16 @@ Bónusz feladat (5 pont)
 
 Adj hozzá adatokat mindegyik táblához!
 (Az adatoknak nem kell valósnak lenniük. Egy felhasználói email-cím lehet például: 'valami@valami.va')
+
+INSERT INTO `user_info`(`name`, `email`, `password`, `isActive`)
+VALUES ('valaki', 'valaki@email.com', '123456a', 1),
+('valaki más', 'valakimas@email.com', '123456b', 1),
+('inaktiv', 'inaktiv@email.com', '123456c', 0);
+
+INSERT INTO `message_info`(`message`)
+VALUES ('juhuuu, ez egy üzenet');
+
+INSERT INTO `message_user`(`fromID`, `toID`, `messageID`, `isReply`)
+VALUES (1, 2, 1, 0);
 
 */
